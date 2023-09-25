b7d25fccdc21e9a65b7efb8788374ad9dc92468b
segabaser <139810447+segabaser@users.noreply.github.com>
2023-09-25 09:01:19 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,16 @@
+}
+    const data = generateRandomData();
+    });
+        console.log("Random Number: " + item);
+
+}
+function main() {
+    for (let i = 0; i < 10; i++) {
+    const data = [];
+    data.forEach(item => {
+    return data;
+main();
+
+function generateRandomData() {
+    }
+        data.push(Math.floor(Math.random() * 100) + 1);
