4de02c9cfa5e3d0081d1d4d3471862f44ccb8d8d
segabaser <139810447+segabaser@users.noreply.github.com>
2023-09-27 16:29:50 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,14 +1,55 @@
-if __name__ == "__main__":
-    data = generate_random_data()
-def main():
-    data = [random.randint(1, 100) for _ in range(10)]
-    main()
+    public static void main(String[] args) {
+    }
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+        int[] data = new int[10];
+        }
 
+}
 
-        print(f"Random Number: {item}")
-
+            System.out.println("Random Number: " + item);
+        }
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;    data = generate_random_data()
     for item in data:
-def generate_random_data():
-import random
 
     return data
+
+def main():
+import random
+
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+
+        print(f"Random Number: {item}")
+def generate_random_data():    data = [random.randint(1, 100) for _ in range(10)]
+
+    return data
+        print(f"Random Number: {item}")
+    for item in data:
+def main():
+
+
+def generate_random_data():
+if __name__ == "__main__":
+    main()
+
+    data = generate_random_data()
+import random    return data;
+    });
+}
+    }
+
+
+    for (let i = 0; i < 10; i++) {
+function generateRandomData() {
+function main() {
+    const data = [];
+        console.log("Random Number: " + item);
+
+}
+main();
+        data.push(Math.floor(Math.random() * 100) + 1);
+    data.forEach(item => {
+    const data = generateRandomData();
