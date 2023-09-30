1640e8261b897c87303f3ff14de99b42255ed1f9
segabaser <139810447+segabaser@users.noreply.github.com>
2023-09-30 18:23:41 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,17 +1,40 @@
-    return data;
-}
-    const data = generateRandomData();
-function main() {
-main();
-        data.push(Math.floor(Math.random() * 100) + 1);
-    }
+    main()
 
-}
-    for (let i = 0; i < 10; i++) {
+    for item in data:
 
-    const data = [];
+    data = generate_random_data()
+def main():
+if __name__ == "__main__":
+    return data
 
-        console.log("Random Number: " + item);
-function generateRandomData() {
-    data.forEach(item => {
-    });
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+import random
+
+def generate_random_data():def main():
+    return data
+    main()
+def generate_random_data():
+    data = generate_random_data()
+
+        print(f"Random Number: {item}")
+
+
+    for item in data:
+
+if __name__ == "__main__":
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+if __name__ == "__main__":
+    main()
+import random
+        print(f"Random Number: {item}")
+def generate_random_data():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    return data
+    data = generate_random_data()
+
+def main():
