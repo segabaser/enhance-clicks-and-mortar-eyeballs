02c6fab4931116ae297e6d95dd7c084903f9fed8
segabaser <139810447+segabaser@users.noreply.github.com>
2023-10-02 07:22:00 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,55 +1,44 @@
-    public static void main(String[] args) {
-    }
-        for (int i = 0; i < 10; i++) {
-public class RandomDataGenerator {
-        int[] data = new int[10];
+            data[i] = (int) (Math.random() * 100) + 1;
         }
 
-}
 
             System.out.println("Random Number: " + item);
         }
+public class RandomDataGenerator {
         for (int item : data) {
-            data[i] = (int) (Math.random() * 100) + 1;    data = generate_random_data()
-    for item in data:
-
-    return data
-
-def main():
-import random
-
-    main()
-    data = [random.randint(1, 100) for _ in range(10)]
-if __name__ == "__main__":
-
-        print(f"Random Number: {item}")
-def generate_random_data():    data = [random.randint(1, 100) for _ in range(10)]
-
-    return data
-        print(f"Random Number: {item}")
-    for item in data:
-def main():
-
-
-def generate_random_data():
-if __name__ == "__main__":
-    main()
-
-    data = generate_random_data()
-import random    return data;
-    });
 }
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
     }
-
-
+    }
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
     for (let i = 0; i < 10; i++) {
-function generateRandomData() {
-function main() {
+    const data = generateRandomData();
+
     const data = [];
+function main() {
+    data.forEach(item => {
         console.log("Random Number: " + item);
+main();
 
 }
-main();
+function generateRandomData() {
+    });
+    return data;function main() {
+
         data.push(Math.floor(Math.random() * 100) + 1);
-    data.forEach(item => {
+    for (let i = 0; i < 10; i++) {
+    return data;
+}
     const data = generateRandomData();
+        console.log("Random Number: " + item);
+    }
+function generateRandomData() {
+}
+    data.forEach(item => {
+
+    const data = [];
+    });
+main();
