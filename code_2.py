c8c17099d6ac4eec728d0895472bd4e7a9c6fe73
segabaser <139810447+segabaser@users.noreply.github.com>
2023-10-08 11:17:46 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,40 +1,52 @@
+
+if __name__ == "__main__":
     return data
-        print(f"Random Number: {item}")
     for item in data:
-def main():
 import random
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+
     main()
 
-    data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
-def generate_random_data():
-
-if __name__ == "__main__":
-
-    data = [random.randint(1, 100) for _ in range(10)]
-
-
-    data = generate_random_data()
-if __name__ == "__main__":
-    for item in data:
-
-import random
-    main()
-    return data
-
-def generate_random_data():
-def main():
-        print(f"Random Number: {item}")    main()
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data
-import random
-
-if __name__ == "__main__":
-def generate_random_data():
-    data = generate_random_data()
-
-def main():
-
         print(f"Random Number: {item}")
+def main():
 
+def generate_random_data():
+    data = generate_random_data()
+import random
     for item in data:
+def generate_random_data():
+if __name__ == "__main__":
+
+
+    return data
+
+def main():
+        print(f"Random Number: {item}")
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]    main()
+    return data
+
+def main():
+    data = generate_random_data()
+
+if __name__ == "__main__":
+
+
+import random
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+def generate_random_data():            data[i] = (int) (Math.random() * 100) + 1;
+
+        }
+}
+        }
+    }
+
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+public class RandomDataGenerator {
+        int[] data = new int[10];
